9d22b2f695be5968d6038d9c1846c49a08e1a19e
LEFI ABDELMONEM <lefi0528@hotmail.fr>
2026-04-30 21:35:58 +0100

    chore: bump version to 3.3.0 (fix list_categories full tree + productCount)

--- fexa_ai_connector.php
+++ fexa_ai_connector.php
@@ -41,7 +41,7 @@ class Fexa_ai_connector extends Module
         $this->tab = 'seo';
         $this->need_instance = 0;
         $this->bootstrap = true;
-        $this->version = '3.2.9';
+        $this->version = '3.3.0';
 
         parent::__construct();
 
